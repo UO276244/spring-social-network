8236a7ea0ccc6ac491b67cfcbe20d75d671fc374
Martin Beltran <uo276244@uniovi.es>
2022-03-11 09:53:21 +0100

    Initial commit

---
+++ src/main/java/com/uniovi/sdipractica134/SdiPractica134Application.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdipractica134;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiPractica134Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdiPractica134Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdipractica134/SdiPractica134ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdipractica134;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdiPractica134ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
