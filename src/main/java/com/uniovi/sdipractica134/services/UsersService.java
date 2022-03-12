d4be3473241b2167e3812a6ec5e1f6fd3195fa0a
Celia <uo276077@uniovi.es>
2022-03-12 20:50:24 +0100

    Basic friend list

--- src/main/java/com/uniovi/sdipractica134/services/UsersService.java
+++ src/main/java/com/uniovi/sdipractica134/services/UsersService.java
@@ -3,6 +3,8 @@ package com.uniovi.sdipractica134.services;
 import com.uniovi.sdipractica134.entities.User;
 import com.uniovi.sdipractica134.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
