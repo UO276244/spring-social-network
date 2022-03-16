070fb2af49f52f7b7bb99c0ae0daa6c5e2219742
Sara Fernández Arias <uo269546@uniovi.es>
2022-03-16 10:32:06 +0100

    PostFormValidator,checks for post publication and getPost GET type request addition

--- src/main/java/com/uniovi/sdipractica134/controllers/PostController.java
+++ src/main/java/com/uniovi/sdipractica134/controllers/PostController.java
@@ -2,22 +2,33 @@ package com.uniovi.sdipractica134.controllers;
 
 
 import com.uniovi.sdipractica134.entities.Post;
+import com.uniovi.sdipractica134.entities.User;
 import com.uniovi.sdipractica134.services.PostsService;
+import com.uniovi.sdipractica134.services.UsersService;
+import com.uniovi.sdipractica134.validators.PostFormValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
+import java.util.Date;
 
 @Controller
 public class PostController {
     @Autowired
     private PostsService postsService;
+    @Autowired
+    private UsersService usersService;
+    @Autowired
+    private PostFormValidator postFormValidator;
+
     @RequestMapping("post/list")
     public String listOwnPosts(Model model, Pageable pageable, Principal principal, @RequestParam(value="",required=false)String searchTest){
 
@@ -25,9 +36,22 @@ public class PostController {
     }
 
     @RequestMapping(value="post/add", method= RequestMethod.POST)
-    public String publishPost(@ModelAttribute Post post){
+    public String publishPost(Principal principal, @Validated Post post, BindingResult result){
+        postFormValidator.validate(post, result);
+        if(result.hasErrors()){
+            return "post/add";
+        }
+
+        post.setDateOfCreation(new Date());
+        User owner=usersService.getUserByEmail(principal.getName());
+
+        post.setOwner(owner);
         postsService.addNewPost(post);
 
-        return "home";
+        return "redirect:/post/list";
+    }
+    @RequestMapping("post/add")
+    public String getPost(){
+        return "post/add";
     }
 }
---
+++ src/main/java/com/uniovi/sdipractica134/validators/PostFormValidator.java
@@ -0,0 +1,25 @@
+package com.uniovi.sdipractica134.validators;
+
+import com.uniovi.sdipractica134.entities.Post;
+import com.uniovi.sdipractica134.services.PostsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+@Component
+public class PostFormValidator implements Validator {
+    @Autowired
+    private PostsService postsService;
+    @Override
+    public boolean supports(Class<?> clazz){return Post.class.equals(clazz);}
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Post post=(Post) target;
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"title","Error.empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"description","Error.empty");
+
+    }
+
+}
