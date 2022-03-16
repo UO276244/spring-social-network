901275688a8f3f02f43fa0ad22ac2b4432f966e7
Sara Fernández Arias <uo269546@uniovi.es>
2022-03-16 09:55:44 +0100

    PostController post/add request implementation. +Solved some errors in messages. +Added new messages related to posts.

--- src/main/java/com/uniovi/sdipractica134/repositories/PostsRepository.java
+++ src/main/java/com/uniovi/sdipractica134/repositories/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.uniovi.sdipractica134.repositories;
 
 import com.uniovi.sdipractica134.entities.Post;
+import com.uniovi.sdipractica134.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostsRepository extends CrudRepository<Post, Long> {
+    @Query("SELECT p FROM Post p WHERE p.owner = ?1 ORDER BY p.dateOfCreation DESC")
+    Page<Post> findAllByUser(Pageable pageable, User user);
 }
